4937162527ddc8ac07148cb8400d41f9e8d8744f
makingglitches <john.r.sohn@gmail.com>
2025-07-16 14:13:58 -0400

    refs #146 	* added module to generate insert statements

--- bulkchunk.py
+++ bulkchunk.py
@@ -29,20 +29,50 @@ def genstatement(sql:str, parameters:list[dict], tupleLimit=200)->list[str]:
     # single instance of unnamed parameters (?,..,?)
     valpiece = "("+ ','.join( ['?' for i in range(0,len(collist))]) +')'
 
-    maxpieces = math.trunc( maxparamlim / collist) 
+    maxpieces = math.trunc( maxparamlim / len(collist)) 
 
     # will error if there is none
     table =  sql[0: sql.index('(')-1].strip().split(' ')[-1]
         
     if maxpieces > tupleLimit:
         maxpieces = tupleLimit
+
+    if len(parameters) > maxpieces:
+        raise ValueError("Adjust your logic, for the query provided maximum parameters is exceeded by the list of values supplied")
+    elif len(parameters) < maxpieces:
+        maxpieces = len(parameters)    
     
-    basesql = f"INSERT INTO {table}( {','.join(collist)}) VALUES ( "
+    basesql = f"INSERT INTO {table}( {','.join(collist)}) VALUES (\n" 
+    basesql = basesql +  ',\t\n'.join( [ valpiece for i in range(0,maxpieces) ]) + "\n);"
 
-
+    return basesql
+                        
 
 if __name__=="__main__":
 
     sql = "INSERT INTO users (id, name, age) VALUES (?, ?, ?);"
 
+    params = [
+                {'id':1, 'name':'john', 'age':85},
+                {'id':1, 'name':'moses', 'age':110},
+                {'id':1, 'name':'aensley', 'age':80},
+                {'id':1, 'name':'allan', 'age':115}                
+             ]
+
     print (getColumnList(sql))
+
+    insql = ""
+
+    # fails
+    try:
+        insql = genstatement(sql,params,tupleLimit=2)
+    except ValueError:
+        insql = ""
+
+    # succeeds
+    insql = genstatement(sql,params)
+
+    print(insql)
+
+
+
